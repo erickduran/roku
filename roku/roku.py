69fba0e5fa74aea5970869758b37a45cc31f72be
Alfa Venegas <alfa.venegas@cetys.edu.mx>
2018-10-15 19:13:59 -0700

    implemented click

--- roku/roku.py
+++ roku/roku.py
@@ -6,6 +6,7 @@
 """
 import sys
 import os
+import click
 
 from lexical.lexical_analyser import LexicalAnalyser
 
@@ -13,14 +14,11 @@ dir = os.path.dirname(__file__)
 lexical_categories_path = os.path.join(dir, 'resources/lexical_categories.json')
 syntax_rules_path = os.path.join(dir, 'resources/syntax_rules.json')
 
-
-def main():
-
-	if len(sys.argv) <= 1:
-		print('Please provide an input file. Usage: python3 roku.py input.fang')
-		return
-
-	file = sys.argv[1]
+@click.command()
+@click.argument('source_file')
+@click.option('--output_file', '-o', help='Name of the output file.')
+def main(source_file, output_file):
+	file = source_file
 
 	print('Creating analyser...')
 	analyser = LexicalAnalyser()
