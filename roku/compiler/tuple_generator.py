e28439c686c59222f5ba120aacb2c8ef0cade4d8
Daniel Ortega <danort.97@gmail.com>
2018-12-06 12:23:01 -0800

    finished?

--- roku/compiler/tuple_generator.py
+++ roku/compiler/tuple_generator.py
@@ -5,10 +5,9 @@ from entities import Node
 class TupleGenerator:
 	
 	def __init__(self):
-		self.temp_count = 1
-		self.label_count = 1
+		self.temp_count = 0
+		self.label_count = 0
 		self.tuples = [[]]
-		pass
 
 	def create_function_tuples(self, parent):
 		context = [parent.children[1].core[2]]
@@ -44,22 +43,11 @@ class TupleGenerator:
 
 		return tuples
 
-		# if (parent.core == ':functions'):
-		# 	functions = []
-		# 	functions.append(self.create_tuples(parent.children[0]))
-			
-		# 	child = parent
-		# 	while child.rule == 0:
-		# 		child = child.children[1]
-		# 		functions.append(self.create_tuples(child.children[0]))
-
-		# 	return functions
-
 	def create_instruction_tuples(self, parent, context):
 		if parent.rule == 0:
 			return self.assignment_tuple(self.get_value(parent.children[1], context), context[0] + '.result')
 		if parent.rule == 4:
-			pass
+			return ('cabbage', None, None, None)
 
 		else:
 			return self.get_instruction_tuples(parent.children[0], context)
@@ -68,13 +56,16 @@ class TupleGenerator:
 		if parent.core == ':declaration':
 			return self.declare(parent, context)
 		if parent.core == ':call':
-			self.create_function_call_tuples(parent, context)
+			return self.create_function_call_tuples(parent, context)
 		if parent.core == ':assignment':
 			return self.assignment_tuple(self.get_value(parent.children[2], context), parent.children[0].core[2])
 		if parent.core == ':flow':
-			self.if_block(parent, context)
+			return self.if_block(parent, context)
 		if parent.core == ':loop':
-			pass
+			if parent.rule == 0:
+				return self.for_block(parent.children[0], context)
+			if parent.rule == 1:
+				return self.while_block(parent.children[0], context)
 
 	def program_tuples(self, parent, context):
 		context.append(self.create_instruction_tuples(parent.children[0], context))
@@ -83,7 +74,6 @@ class TupleGenerator:
 		while child.rule == 0:
 			child = child.children[1]
 			context.append(self.create_instruction_tuples(child.children[0], context))
-		# return context
 
 	def assignment_tuple(self, value, name):
 		return ('assign', value, '-', name)
@@ -114,7 +104,7 @@ class TupleGenerator:
 					arg_count += 1
 
 		# Make call
-		context.append(self.run_tuple(node.children[0].core[2]))
+		return self.run_tuple(node.children[0].core[2])
 
 	def get_value(self, node, context):
 		if node.rule == 0: # Number
@@ -131,7 +121,7 @@ class TupleGenerator:
 			return func_call.children[0].core[2] + '.result'
 
 		elif node.rule == 4: #logical
-			pass
+			return self.create_logical_tuples(node.children[0], context)
 
 		elif node.rule == 5:
 			return node.children[0].core[1]
@@ -139,10 +129,63 @@ class TupleGenerator:
 		else: # Any other constant or identifier
 			return node.children[0].core[2]
 
-	def get_logical(self, node, context):
-		if 
-		pass
+	def create_logical_tuples(self, node, context): 
+		if node.rule == 0: #log operation 
+			logical_op = node.children[0]
+			return self.create_logical_operation(logical_op, context)
+			
+		if node.rule == 1: #logical value
+			return self.get_logical_value(node.children[0], context)
+
+	def create_logical_operation(self, node, context):
+		if node.rule == 0: #one element
+			element_1 = self.get_logical_value(node.children[0], context)
+			element_2 = self.get_logical_value(node.children[2], context)
+			result = self.next_temp()
+			context.append((node.children[1].core[2], element_1, element_2, result))
+			return result 
+		else: #more elements
+			element_1 = self.get_logical_value(node.children[0], context)
+			element_2 = self.create_logical_operation(node.children[2], context)
+			result = self.next_temp()
+			context.append((node.children[1].core[2], element_1, element_2, result))
+			return result
+
+	def get_logical_value(self, node, context):
+		if node.rule == 0: #comparison
+			element_1 = self.get_comparing(node.children[0].children[0], context)
+			element_2 = self.get_comparing(node.children[0].children[2], context)
+			result = self.next_temp()
+			context.append((node.children[0].children[1].core[2], element_1, element_2, result))
+			return result
+		if node.rule == 1: # negation 
+			original_result = self.create_logical_tuples(node.children[0].children[1], context)
+			negated_result = self.next_temp()
+			context.append(('not', original_result, None, negated_result))
+			return negated_result
+		if node.rule == 2: # koi
+			return node.children[0].core[2]
+		if node.rule == 3: # identifier
+			return node.children[0].core[2]
+		if node.rule == 4: # call
+			func_call = node.children[0]
+			self.create_function_call_tuples(func_call, context)
+			return func_call.children[0].core[2] + '.result'
 	
+	def get_comparing(self, node, context):
+		if node.rule == 0: # Number
+			return node.children[0].children[0].core[2]
+		if node.rule == 2: # Call
+			func_call = node.children[0]
+			self.create_function_call_tuples(func_call, context)
+			return func_call.children[0].core[2] + '.result'
+		elif node.rule == 5: # operation
+			return self.operation(node.children[0], context)
+		elif node.rule == 6: # sokka
+			return node.children[0].core[1]
+		else: #anything else
+			return node.children[0].core[2]
+
 	def operation(self, operation, context):
 		element_1 = self.get_operating(operation.children[0], context)
 		element_2 = self.get_operating(operation.children[2], context)
@@ -154,38 +197,81 @@ class TupleGenerator:
 		if node.rule == 0: # Number
 			return node.children[0].children[0].core[2]
 		elif node.rule == 2: # Call
-			pass
+			func_call = node.children[0]
+			self.create_function_call_tuples(func_call, context)
+			return func_call.children[0].core[2] + '.result'
 		else: # Anything else
 			return node.children[0].core[2]
 
-	def while_block(self, node):
-		pass
-
-	def if_block(self, node, context):
-		self.create_agni_tuples(node.children[0], context)
-		if node.rule == 0:
-			self.create_agnikai_tuples(node.children[1], context)
-			self.create_kai_tuples(node.children[2], context)
-		if node.rule == 1:
-			self.create_agnikai_tuples(node.children[1], context)
-		if node.rule == 2:
-			self.create_kai_tuples(node.children[1])
-
-	def create_agnikai_tuples(self, node, context):
-		agnikai = node.children[0]
+	def while_block(self, node, context):
 		start_label = self.next_label()
 		end_label = self.next_label()
 		
-		context.append(('iftrue', self.get_logical(agnikai.children[2], context), None, start_label))
-		context.append(('iffalse', self.get_logical(agnikai.children[2], context), None, end_label))
+		condition = self.create_logical_tuples(node.children[2], context)
+		context.append(('iftrue', condition, None, start_label))
+		context.append(('iffalse', condition, None, end_label))
+
+		context.append(start_label)
+		self.program_tuples(node.children[5], context)
+		condition = self.create_logical_tuples(node.children[2], context)
+		context.append(('iftrue', condition, None, start_label))
+		return end_label
+
+	def for_block(self, node, context):
+		start_label = self.next_label()
+		end_label = self.next_label()
+
+		context.append(self.get_instruction_tuples(node.children[2], context))
+
+		condition = self.create_logical_tuples(node.children[4], context)
+
+		context.append(('iftrue', condition, None, start_label))
+		context.append(('iffalse', condition, None, end_label))
+
+		context.append(start_label)
+		self.program_tuples(node.children[9], context)
+		context.append(self.get_instruction_tuples(node.children[6], context))
+
+		condition = self.create_logical_tuples(node.children[4], context)
+		context.append(('iftrue', condition, None, start_label))
+
+		return end_label
+		
+
+	def if_block(self, node, context):
+		self.create_agni_tuples(node.children[0], context)
+		agni_end_label = self.current_label()
+		block_end_label = self.next_label()
+		context.append(('goto', None, None, block_end_label))
+		context.append(agni_end_label)
+
+		if node.rule == 0:
+			self.create_agnikai_tuples(node.children[1], context, block_end_label)
+			self.create_kai_tuples(node.children[2], context)
+		if node.rule == 1:
+			self.create_agnikai_tuples(node.children[1], context, block_end_label)
+		if node.rule == 2:
+			self.create_kai_tuples(node.children[1])
+
+		return block_end_label
+
+	def create_agnikai_tuples(self, node, context, block_end):
+		agnikai = node.children[0]
+		start_label = self.next_label()
+		end_label = self.next_label()
+		condition = self.create_logical_tuples(agnikai.children[2], context)
+		context.append(('iftrue', condition, None, start_label))
+		context.append(('iffalse', condition, None, end_label))
 
 		context.append(start_label)
 		if agnikai.rule == 0:
 			self.program_tuples(agnikai.children[5], context)
+		context.append(('goto', None, None, block_end))
 		context.append(end_label)
+
 		
 		if node.rule == 1:
-			self.create_agnikai_tuples(node.children[1], context)
+			self.create_agnikai_tuples(node.children[1], context, block_end)
 
 	def create_kai_tuples(self, node, context):
 		if node.rule == 0:
@@ -194,23 +280,27 @@ class TupleGenerator:
 	def create_agni_tuples(self, node, context):
 		start_label = self.next_label()
 		end_label = self.next_label()
-		context.append(('iftrue', self.get_logical(node.children[2], context), None, start_label))
-		context.append(('iffalse', self.get_logical(node.children[2], context), None, end_label))
+		condition = self.create_logical_tuples(node.children[2], context)
+		context.append(('iftrue', condition, None, start_label))
+		context.append(('iffalse', condition, None, end_label))
 
 		context.append(start_label)
 		if node.rule == 0:
 			self.program_tuples(node.children[5], context)
-		context.append(end_label)
+
 
 	def next_temp(self):
-		temp = 'T' + str(self.temp_count)
 		self.temp_count += 1
+		temp = 'T' + str(self.temp_count)
 		return temp
 
 	def next_label(self):
-		label = 'L' + str(self.label_count)
 		self.label_count += 1
+		label = 'L' + str(self.label_count)
 		return label
+	
+	def current_label(self):
+		return 'L' + str(self.label_count)
 
 	def run_tuple(self, label):
 		return ('run', None, None, label)
